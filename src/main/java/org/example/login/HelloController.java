3aee5e646c158effb9389823d071507ea5f8b4ef
Aathif <aathifzacky6@gmail.com>
2024-02-16 17:31:38 +0530

    Initial commit

---
+++ src/main/java/org/example/login/HelloController.java
@@ -0,0 +1,46 @@
+package org.example.login;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class HelloController {
+    @FXML
+    private Label lbllogin;
+
+    @FXML
+    private TextField txtusername;
+
+    @FXML
+    private TextField txtpassword;
+
+    public void Login(ActionEvent event) {
+        if (txtusername.getText().equals("User") && txtpassword.getText().equals("Pass")) {
+            lbllogin.setText("Login Success");
+        } else {
+            lbllogin.setText("Login Failed");
+        }
+    }
+
+    @FXML
+    private void handleCreateButtonAction() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("signup.fxml"));
+            Parent parent = fxmlLoader.load();
+            Stage stage = new Stage();
+            Image icon = new Image("icon.png");
+            stage.getIcons().add(icon);
+            stage.setTitle("Signup");
+            stage.setScene(new Scene(parent));
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/org/example/login/SignupController.java
@@ -0,0 +1,21 @@
+package org.example.login;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class SignupController {
+
+    @FXML
+    private Button createButton;
+
+    @FXML
+    public void initialize() {
+        // Initialize any components if necessary
+    }
+
+    @FXML
+    private void handleCreateButtonAction() {
+        // Show the signup form when the "Create" button is pressed
+    }
+}
+
